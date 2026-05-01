ae9d29a3d5d1b5862a1ce4b303810d7e6f3f3fe3
PremanshBehl <premanshbehl978@gmail.com>
2026-05-01 07:51:47 +0530

    chore: fix CORS for production deployment

--- backend/main.py
+++ backend/main.py
@@ -49,6 +49,7 @@ app.add_middleware(
         "http://127.0.0.1:3000",
         "http://127.0.0.1:3001",
     ],
+    allow_origin_regex="https://.*",
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],
     allow_headers=["*"],
